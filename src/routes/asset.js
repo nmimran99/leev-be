86af824f1b48593b1b4bee34cb374c2fdcf5ec74
nmimran99 <nmimran99@gmail.com>
2021-07-03 18:51:35 -0400

    commit

--- src/routes/asset.js
+++ src/routes/asset.js
@@ -2,11 +2,12 @@ import { Router } from 'express';
 import * as controller from '../controller/asset';
 import { authenticate } from '../middleware/authenticate';
 import { authorize } from '../middleware/authorize';
+import { uploadAssetImage } from '../services/multer.service';
 
 const router = Router();
 
-router.post('/createAsset', authenticate, authorize, controller.createAsset);
-router.post('/updateAsset', authenticate, authorize, controller.updateAsset);
+router.post('/createAsset', authenticate, authorize, uploadAssetImage.array('images'), controller.createAsset);
+router.post('/updateAsset', authenticate, authorize, uploadAssetImage.array('images'), controller.updateAsset);
 router.post('/updateAssetOwner', authenticate, authorize, controller.updateAssetOwner);
 router.post('/updateAssetType', authenticate, authorize, controller.updateAssetType);
 router.post('/getAssets', authenticate, authorize, controller.getAssets);
--- src/services/multer.service.js
+++ src/services/multer.service.js
@@ -86,8 +86,29 @@ const  documentStorage = multer.diskStorage({
   }
 })
 
+const  assetStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, 'asset_' + Date.now() + path.extname(file.originalname))
+  },
+  fileFilter: function (req, file, cb) {
+      const fileTypes = /doc|docx|pdf|xlsx|xls|txt|csv|ppt|pptx/;
+      const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+      const mimetpye = fileTypes.test(file.mimetpye);
+      
+      if (mimetpye && extname) {
+          return cb(null, true);
+      } else {
+          cb('File type not allowed');
+      }
+
+  }
+})
+
  export const uploadAvatar  = multer({ storage: avatarStorage });
  export const uploadFaultImage  = multer({ storage: faultStorage });
  export const uploadTaskImage  = multer({ storage: taskStorage });
  export const uploadDocument  = multer({ storage: documentStorage });
- 
+ export const uploadAssetImage = multer({ storage: assetStorage });
